c5922f4fb38606053c24fc92e64b63712e5815d0
B-andir <svenberg.daniel@gmail.com>
2021-11-29 02:25:56 +0100

    Simple CSS and Stylized Dark Mode + basic SQL

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const mysql = require('mysql');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT != null ? process.env.PORT : 5100;
 
 const app = express();
+
+app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,15 +23,25 @@ app.use(session({
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
 
 app.use(morgan('dev'));
 
+var con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("MySQL Database Connected")
+    con.destroy();
+})
 
 // route to requested page
 app.use('/', require('./routes/routing.js'));
 
 
 app.listen(PORT, () => {
-    console.log(`\nServer running on port ${PORT}...\n`);
+    console.log(`\nServer running on port ${PORT}...`);
 });
--- public/scripts/toggleDarkMode.js
+++ public/scripts/toggleDarkMode.js
@@ -1,8 +1,34 @@
-function toggleDarkMode(){
-    const element = document.getElementById("container")
-    if (element.classList.contains("darkMode")){
+var element = document.getElementById("container");
+var currentRotation = 0;
+
+function SetDarkMode() {
+    let cookie = GetCookie("darkMode");
+    
+    if (cookie == "false") {
+        // Disable Dark Mode
         element.classList.remove("darkMode");
-    } else {
-        element.classList.add("darkMode");
+        currentRotation += 180;
     }
+
+    document.querySelector("#darkmodeToggle").style.transform = 'rotate(' + currentRotation + 'deg';
+}
+
+function ToggleDarkMode() {
+
+    document.querySelector("#darkmodeToggle").style.transition = 'transform 0.3s, box-shadow 0.2s';
+    document.querySelector("#container").style.transition = 'background-color 0.3s';
+
+    if (element.classList.contains("darkMode")) {
+        // Disable Dark Mode
+        element.classList.remove("darkMode");
+        document.cookie = "darkMode=false; path=/";
+        currentRotation += 180;
+    } else {
+        // Set Dark Mode
+        element.classList.add("darkMode");
+        document.cookie = "darkMode=true; path=/";
+        currentRotation += 180;
+    }
+
+    document.querySelector("#darkmodeToggle").style.transform = 'rotate(' + currentRotation + 'deg';
 }
