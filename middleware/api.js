fd94e3e9f9a77f59968ebae6837d7f0093350a2a
B-andir <svenberg.daniel@gmail.com>
2021-12-02 15:02:59 +0100

    Image Uploading to Server and Database Functional

--- middleware/api.js
+++ middleware/api.js
@@ -22,60 +22,57 @@ const handleError = (err, res) => {
         .end("Oops! Something went wrong!");
 };
 
-// TODO: Fix this shit. Query already executed: Product.findOne({title: string})
-//       Also, creating new model executes before parsing through the images stuff, which fucks up the image URLs.
-router.post('/build/upload', upload.array('image', 8), async (req, res) => {
+router.post('/build/upload', upload.array('image', 8), (req, res) => {
     let imageURLs = [];
 
-    let test = await productModel.findOne({ title: req.body.title }, () => {
-        for (i = 0; i < req.files.length; i++) {
-            let file = req.files[i];
-            const tempPath = file.path;
-            const targetPath = path.join(__dirname, '../public/images/productImages/', file.originalname);
-            // console.log(req.body);  // req.body is a dictionary of the form submit.
+    console.log(req.files);
+    productModel.findOne({ title: req.body.title }, async (err, product) => {
+        if (!err && !product) {
+            let length = req.files.length;
+            for (i = 0; i < length; i++) {
+                let file = req.files[i];
+                const tempPath = file.path;
+                const targetPath = path.join(__dirname, '../public/images/productImages/', file.originalname);
+                // console.log(req.body);  // req.body is a dictionary of the form submit.
 
-            let pathExtname = path.extname(file.originalname).toLowerCase();
+                let pathExtname = path.extname(file.originalname).toLowerCase();
 
-            if (pathExtname === ".png" || pathExtname === ".jpg") {
-                fs.rename(tempPath, targetPath, err => {
+                if (pathExtname === ".png" || pathExtname === ".jpg") {
+                    fs.renameSync(tempPath, targetPath);
                     if (err) handleError(err, res);
                     imageURLs.push('/public/images/productImages/' + file.originalname);
-                    console.log(imageURLs)
-                });
-            } else {
-                fs.unlink(tempPath, err => {
-                    if (err) return handleError(err, res);
+                } else {
+                    fs.unlinkSync(tempPath, err => {
+                        if (err) return handleError(err, res);
 
-                    console.log(`Error: Only .png and .jpg allowed! [Image ${i}]`);
-                });
+                        console.log(`Error: Only .png and .jpg allowed! [Image ${i}]`);
+                    });
+                }
+
+                if (i == length - 1){
+                    let kind = req.body.kind.toLowerCase(), pattern = req.body.pattern.toLowerCase(), title = req.body.title;
+                    const product = await productModel.create({ 
+                        kind: kind, 
+                        pattern: pattern, 
+                        title: title, 
+                        url: (`${kind}/${pattern}/${title}/`), 
+                        description: req.body.description, 
+                        estimatedPriceKr: req.body.estimatedPriceKr, 
+                        images: imageURLs
+                    }, (err, product) => {
+                        if (err) throw err;
+            
+                        console.log("Created new product!");
+            
+                        res.redirect(('/products/' + product.url));
+                    });
+                }
             }
+            
+        } else if (err) {
+            throw err;
         }
-    });
-    // if (err) throw err;
-    // else if (product != null) {
-    //     console.log("Product already exists!")
-    //     res.redirect('/order/admin/build');
-    // }
-
-    let kind = req.body.kind.toLowerCase(), pattern = req.body.pattern.toLowerCase(), title = req.body.title;
-
-    console.log(imageURLs + "iosdjf");
-    productModel.create({ 
-        kind: kind, 
-        pattern: pattern, 
-        title: title, 
-        url: (`${kind}/${pattern}/${title}/`), 
-        description: req.body.description, 
-        estimatedPriceKr: req.body.estimatedPriceKr, 
-        images: imageURLs
-    }, (err, product) => {
-        if (err) throw err;
-
-        console.log("Created new product!");
-
-        res.redirect(('/products/' + product.url));
-    });
-        
+    }).clone().catch((err) => { console.log(err)});
 });
 
 module.exports = router;
