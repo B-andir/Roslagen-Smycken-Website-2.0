faf4ec48229bd6ce8ccd52de47d3a6002e502638
WinterBeyond <winterexodius@gmail.com>
2022-04-01 09:43:17 +0200

    Cleanup cookie checking

--- middleware/checkCookies.js
+++ middleware/checkCookies.js
@@ -13,74 +13,47 @@ router.use(async (req, res, next) => {
     const cookie = await req.cookies.LOGIN_COOKIE;
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    res.locals.user = {};
+    if (!cookie) return next();
 
-    if (cookie != null) {
-        const decoded = await jwt.verify(
-            cookie,
-            process.env.JWT_SECRET,
-            async (err, decoded) => {
-                if (err) {
-                    console.warn(
-                        "WARNING: User attempted verification with invalid cookie"
-                    );
-                    res.cookie("LOGIN_COOKIE", "", { maxAge: 0 });
-                    throw err;
-                }
+    res.locals.user = null;
 
-                if (decoded.ip != ip) {
-                    console.warn(
-                        "WARNIND: User attempted verification with missmatchin IP"
-                    );
-                    res.cookie("LOGIN_COOKIE", "", { maxAge: 0 });
-                    res.locals.user = null;
-                    next();
-                } else {
-                    const user = await userModel
-                        .findById(decoded.mongoID, (err, user) => {
-                            if (err) {
-                                throw err;
-                            }
+    try {
+        const decoded = jwt.verify(cookie, process.env.JWT_SECRET);
 
-                            if (user.privateKey == decoded.privateKey) {
-                                res.locals.user = {
-                                    firstName: user.firstName,
-                                    lastName: user.lastName,
-                                    avatarURL: user.avatarURL,
-                                    isAdmin: user.isAdmin,
-                                };
+        if (decoded.ip !== ip) {
+            console.warn(
+                "WARNIND: User attempted verification with mismatching IP"
+            );
 
-                                next();
-                            } else {
-                                res.cookie("LOGIN_COOKIE", "", { maxAge: 0 });
+            res.clearCookie("LOGIN_COOKIE");
+            return next();
+        }
 
-                                res.locals.user = null;
-                                next();
-                            }
-                        })
-                        .clone()
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                }
-            }
-        );
-    } else {
-        res.locals.user = null;
-        next();
+        const user = await userModel.findById(decoded.mongoID);
+        if (!user) return next();
+
+        if (user.privateKey == decoded.privateKey) {
+            res.locals.user = {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                avatarURL: user.avatarURL,
+                isAdmin: user.isAdmin,
+            };
+
+            return next();
+        }
+    } catch (error) {
+        throw error;
     }
+
+    return next();
 });
 
 // Check darkMode cookie
 router.use((req, res, next) => {
     const cookie = req.cookies.darkMode;
-    if (cookie == "true") {
-        res.locals.darkMode = true;
-        next();
-    } else {
-        res.locals.darkMode = false;
-        next();
-    }
+    res.locals.darkMode = cookie == "true";
+    next();
 });
 
 module.exports = router;
