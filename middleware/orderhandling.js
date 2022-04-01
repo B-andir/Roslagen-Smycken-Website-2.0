2c561e8961b1633b19398f206e453ff9c2620bdf
WinterBeyond <winterexodius@gmail.com>
2022-04-01 09:53:15 +0200

    Cleanup order handling

--- middleware/orderhandling.js
+++ middleware/orderhandling.js
@@ -2,42 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 const productModel = require("../models/product");
-const { env } = require("process");
 require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 router.post("/buy", async (req, res) => {
-    productModel.findById(req.body.productID, async (err, _product) => {
-        if (err) throw err;
+    const product = await productModel.findById(req.body.productID);
+    if (!product) return res.status(404).send("Product not found");
 
-        const product = await stripe.products.create({
-            name: _product.title,
-            description: _product.description,
-        });
-
-        const price = await stripe.prices.create({
-            unit_amount:
-                _product.estimatedPriceKr * req.body.selectedLength * 100,
-            currency: "sek",
-            product: product.id,
-        });
-
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    price: price.id,
-                    quantity: 1,
-                },
-            ],
-            mode: "payment",
-            success_url:
-                "https://www.roslagensmycken.com/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "https://www.roslagensmycken.com/cancel",
-        });
-
-        return res.json({ targetURL: session.url });
+    const stripePoduct = await stripe.products.create({
+        name: product.title,
+        description: product.description,
     });
+
+    const price = await stripe.prices.create({
+        unit_amount: product.estimatedPriceKr * req.body.selectedLength * 100,
+        currency: "sek",
+        product: stripePoduct.id,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                price: price.id,
+                quantity: 1,
+            },
+        ],
+        mode: "payment",
+        success_url:
+            "https://www.roslagensmycken.com/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "https://www.roslagensmycken.com/cancel",
+    });
+
+    return res.json({ targetURL: session.url });
 });
 
 module.exports = router;
